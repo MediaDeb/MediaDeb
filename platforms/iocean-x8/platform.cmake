2fcbcbd95848fc33a066f28a0c303a16bee936cc
Andrew Andrianov <andrew@ncrmnt.org>
2017-08-01 16:13:03 +0300

    buildsystem: Switch iocean-x8 to cmake, clean up things
    Signed-off-by: Andrew Andrianov <andrew@ncrmnt.org>

---
+++ platforms/iocean-x8/platform.cmake
@@ -0,0 +1,14 @@
+set(GNU_TARGET_NAME        "arm-module-linux-gnueabi")
+
+set(DEVICE_MAINTAINER      "Andy 'ncrmnt' <spam@ncrmnt.org>")
+set(DEVICE_DISTRO          "debian")
+set(DEVICE_DEBARCH         "armhf")
+set(DEVICE_ROOT_VARIANT    "basic")
+set(DEVICE_KERNEL_VERSION  "3.4.67")
+
+set(DEVICE_CRAPWARE_TYPE   "MediaTek")
+set(DEVICE_MTK_TARGET      "wiko")
+
+set(DEVICE_CRAPWARE_NAME    "MT6592-KK-KERNEL.git")
+set(DEVICE_CRAPWARE_GIT     "https://github.com/MediaDeb/${DEVICE_CRAPWARE_NAME}")
+set(DEVICE_CRAPWARE_BRANCH  "iocean-x8")
